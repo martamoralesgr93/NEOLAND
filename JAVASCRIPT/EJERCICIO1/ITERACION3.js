731a61d57fd3f6237870fe8622976eca7b440d6e
Marta Morales <mmoralesgr93@gmail.com>
2024-03-21 18:41:35 +0100

    ejercicios

---
+++ JAVASCRIPT/EJERCICIO1/ITERACION3.js
@@ -0,0 +1,43 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+let primerNumero = 10
+let segundoNumero = 5
+console.log(primerNumero);
+console.log(primerNumero * segundoNumero);
+
+
+ 
+
+primerNumero *= segundoNumero;
+console.log(primerNumero);
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+let tercerNumero = 2
+
+console.log(primerNumero / tercerNumero);
+
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+let cuartoNumero = 15
+let quintoNumero = 9
+
+console.log(cuartoNumero % quintoNumero);
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15,
+// teniendo dos variables p = 10 y j = 5.
+
+let p = 10;
+let j = 5;
+let o = p + j;
+
+console.log(o);
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+
+let c = 10;
+let m = 5;
+let i = c * m;
+
+console.log(i);
