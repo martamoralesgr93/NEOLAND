8a41c2b285298ee50362ec64dc43c83bd62800eb
mmoralesgr93 <mmoralesgr93@gmail.com>
2024-04-30 18:02:33 +0200

    dispersa1

--- REACT/jsx-basics/src/App.jsx
+++ REACT/jsx-basics/src/App.jsx
@@ -17,6 +17,11 @@ const GreetingComponent = () => {
       return 'Buenas noches';
     }
   };
+ // Array de frutas
+ const fruits = ['Manzana', 'Plátano', 'Naranja', 'Pera', 'Uva'];
+
+ // Aquí creo el botón de true o falsa
+ const [isVisible, setIsVisible] = useState(true);
 
   return (
     <>
@@ -45,12 +50,29 @@ const GreetingComponent = () => {
         <p>
           {determineGreeting(count)}
         </p>
-      </div>
+        </div>
       <p className="read-the-docs">
         Click en los logotipos de Vite y React para aprender más
       </p>
-    </>
-  );
-};
+      <ul>
+        {fruits.map((fruit, index) => ( // Utilizamos el método map porque los elementos son de tipo JSX (osea dinámicos)
+          <li key={index}>{fruit}</li>))}
+      </ul>
+        
+       {/* Cuando clickas te sale contenido */}
+       <button onClick={() => setIsVisible(!isVisible)}>
+        {isVisible ? 'Ocultar Contenido' : 'Mostrar Contenido'}
+      </button>
+
+      {/* para rendirizar o pintar el contenido */}
+      {isVisible && (
+        <div className="content">
+          <h2>Contenido Visible</h2>
+          <p>Este es el contenido que se muestra cuando isVisible es true.</p>
+        </div>
+      )}
+    </>)
+}
+
 
 export default GreetingComponent;
